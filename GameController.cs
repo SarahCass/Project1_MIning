52f0ca30514c45d60eb7232f3378c12b62aa970e
SarahCass <55501862+SarahCass@users.noreply.github.com>
2019-09-23 01:27:25 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    bool threeSecondAlert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        threeSecondAlert = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > 3 && threeSecondAlert)
+        {
+            print("It's been 3 seconds!");
+            threeSecondAlert = false;
+        }
+        
+    }
+}
