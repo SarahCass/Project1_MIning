dc53db9ade03918d96c48e4ebe20647194c03ac7
SarahCass <55501862+SarahCass@users.noreply.github.com>
2019-09-23 02:13:35 -0400

    Add files via upload

---
+++ GameControllerCodePt2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    bool threeSecondAlert;
+    int bronzeOre;
+    int silverOre;
+    float miningSpeed;
+    float miningNow;
+    int bronzeSupply;
+    int silverSupply;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        threeSecondAlert = true;
+        bronzeOre = 0;
+        silverOre = 0;
+        miningSpeed = 3;
+        miningNow = miningSpeed;
+        bronzeSupply = 3;
+        silverSupply = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > miningNow)
+        {
+            if (bronzeSupply > 0)
+            {
+                bronzeOre++;
+                bronzeSupply--;
+
+            }
+            else if(silverSupply > 0)
+            {
+                silverOre++;
+                silverSupply--;
+            }
+            miningNow += miningSpeed;
+            print("Bronze:"+bronzeOre+" Silver:"+silverOre);
+        }
+        
+    }
+}
